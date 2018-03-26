a55475d1afb16c7516004be361ef14fe6a564414
Kyrylo Yakovliev <dotsquid@gmail.com>
2018-03-26 21:53:30 +0300

    Improve UI; minor code clean-up

--- Editor/ChronoHelper.cs
+++ Editor/ChronoHelper.cs
@@ -1,5 +1,5 @@
 ﻿/*
-    Copyright (c) 2016 Kyrylo Yakovliev
+    Copyright (c) 2016 - 2018 Kyrylo Yakovliev
     https://twitter.com/dotsquid
     
     This software is provided 'as-is', without any express or implied
@@ -30,6 +30,8 @@ public class ChronoHelper : EditorWindow
         private ChronoHelper _owner;
         private GUIContent _editModeContent;
         private GUIContent _playModeContent;
+        private GUIContent _editModePressedContent;
+        private GUIContent _playModePressedContent;
         private float _value;
         private bool _currentState = false;
         private bool _oldState = false;
@@ -54,7 +56,7 @@ public class ChronoHelper : EditorWindow
             }
         }
 
-        public GUIContent content
+        private GUIContent normalContent
         {
             get
             {
@@ -62,20 +64,38 @@ public class ChronoHelper : EditorWindow
             }
         }
 
-        public ChronoButton(ChronoHelper owner, float value, string title, string tooltip = "")
+        private GUIContent pressedContent
         {
-            _owner = owner;
-            _value = value;
-            _playModeContent = new GUIContent(title);
-            _editModeContent = new GUIContent(title, kEditModeTooltip);
+            get
+            {
+                return _owner._isPlayMode ? _playModePressedContent : _editModePressedContent;
+            }
         }
 
-        public ChronoButton(ChronoHelper owner, float value, GUIContent content)
+        private GUIContent content
+        {
+            get
+            {
+                return state ? pressedContent : normalContent;
+            }
+        }
+
+        public ChronoButton(ChronoHelper owner, float value, GUIContent normalContent, GUIContent pressedContent = null)
         {
             _owner = owner;
             _value = value;
-            _playModeContent = new GUIContent(content);
-            _editModeContent = new GUIContent(content);
+            _playModeContent = new GUIContent(normalContent);
+            _editModeContent = new GUIContent(normalContent);
+            if (null == pressedContent)
+            {
+                _playModePressedContent = new GUIContent(normalContent);
+                _editModePressedContent = new GUIContent(normalContent);
+            }
+            else
+            {
+                _playModePressedContent = new GUIContent(pressedContent);
+                _editModePressedContent = new GUIContent(pressedContent);
+            }
             _editModeContent.tooltip = kEditModeTooltip;
         }
 
@@ -118,30 +138,51 @@ public class ChronoHelper : EditorWindow
     private const string kOneAndHalfButtonTitle = "×1½";
     private const string kTwiceButtonTitle = "×2";
     private const string kEditModeTooltip = "Does not affect Time.timeScale while in EditorMode";
-    private const string kResetIconBase64 = "iVBORw0KGgoAAAANSUhEUgAAAA0AAAAMCAYAAAC5tzfZAAAAXklEQVQoz52RwQ3AMAgDz1GGCfsPk3HcVyTUqmmoXzw4jGzZZikiDGjOyU7tBhypVQEAjTFKAKBGXe6AgOymHZCDUOk/20REvvQa+QqsAaSlI8e+hq9CH1C1pz+R6wKUNx2CpAeEkwAAAABJRU5ErkJggg==";
-    private const string kPauseIconBase64 = "iVBORw0KGgoAAAANSUhEUgAAAAcAAAAMCAYAAACulacQAAAAKUlEQVQY02PU0ND4zwABjNevX2dgYGBg0NTU/M/AwMDAxIAHjEoSkgQANSEFj9cbB0UAAAAASUVORK5CYII=";
+
+    public const string kResetIcon_dark = "iVBORw0KGgoAAAANSUhEUgAAAA0AAAAMCAYAAAC5tzfZAAAAXklEQVQoz52RwQ3AMAgDz1GGCfsPk3HcVyTUqmmoXzw4jGzZZikiDGjOyU7tBhypVQEAjTFKAKBGXe6AgOymHZCDUOk/20REvvQa+QqsAaSlI8e+hq9CH1C1pz+R6wKUNx2CpAeEkwAAAABJRU5ErkJggg==";
+    public const string kResetIcon_light = "iVBORw0KGgoAAAANSUhEUgAAAA0AAAAMCAYAAAC5tzfZAAAAXklEQVQoz52RwQ3AMAgDz1G2yf6bhHncVyTUqmmoXzw4jGzZZikiDGiMwU7tBhypVQEAzTlLAKBGXe6AgOymHZCDUOk/20REvvQa+QqsAaSlI8e+hq9CH1C1pz+R6wLdSyOOjGmt7wAAAABJRU5ErkJggg==";
+    public const string kPauseIcon_dark = "iVBORw0KGgoAAAANSUhEUgAAAAcAAAAMCAYAAACulacQAAAAKUlEQVQY02PU0ND4zwABjNevX2dgYGBg0NTU/M/AwMDAxIAHjEoSkgQANSEFj9cbB0UAAAAASUVORK5CYII=";
+    public const string kPauseIcon_light = "iVBORw0KGgoAAAANSUhEUgAAAAcAAAAMCAYAAACulacQAAAAKUlEQVQY02O8cuXKfwYIYNTW1mZgYGBguHr16n8GBgYGJgY8YFSSkCQA3ocHkwSoErMAAAAASUVORK5CYII=";
+    public const string kLockedIcon_light = "iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAbklEQVQoz52Q0Q2AMAhE70yHgdl0HDsb3QZ/rGkJjdH7g0LvHXR3fFGJjdZa/IEAICLzAkmYmavqMU7fPXaSkrjWUJ9jseGjHquEfZKI8JdDuqCqRwz/lqEm4ZdXgpn5K9IKIb49VyIJAPsKsc9d5Pwp8+BxwkcAAAAASUVORK5CYII=";
+    public const string kUnlockedIcon_dark = "iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAa0lEQVQoz5VRQQ7AIAgT46fkS/odv9RvdYc5o7hM1oRLFdqCkAx/kCyhqssEAKKqAcBNkByVcybJMlfnxp/0otqMwvIYHbaXAfKEtt4tAIhX4WwJQAVQ3Q3dd3Pd4ZQnzjY+AtdtSx3ltNoLI+dJPoQHb3kAAAAASUVORK5CYII=";
+    public const string kUnlockedIcon_light = "iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAcUlEQVQoz5WRyw3AIAxD44ph7NnadTob2YZeCoKoCJpbvn5JUEqxP5ZiwN2HCSTh7kZybABgOeci6eob3hgqSfpQvXtH0pA8NrCHAU0qskcjiV2FNZKkKy6/2uGO7NM/1FMukWYIMdeuBMDM7Jwh1roHbuItb9B5vFoAAAAASUVORK5CYII=";
+    public const string kAutoResetOnIcon_light = "iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAg0lEQVQoz5WQMQ7DMAwDqaJ6iCc9LnlO/ThNfggHZkgcxGkLxDcJMCnKNEmY4YVJ3gDQWgMAkJS7G0m4+yAspYwJJPU44SruM8lT5O42GCJizcxPn+9bM1MAdtOlpeV4wC+DJEjaEw5qREzXWh99+nbrFxGx9tNNEszOEpY/i+tgmGEDIxNHz8eqd30AAAAASUVORK5CYII=";
+    public const string kAutoResetOffIcon_dark = "iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAdElEQVQoz5WQ0Q2DMAwFXxBL2SuFcehKXuv4AWRM2oKlSHZyL4muAXpTk17WJeDuw+fy/vQEjojlmBswhCOiJfhzHgACuplhZgD9mPdeey/gDChByvCvgOrNfwMjuAbmasPdV0lrEXC3lGz0gd27pfqNb2sD55vTX0C1gsEAAAAASUVORK5CYII=";
+    public const string kAutoResetOffIcon_light = "iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAfElEQVQoz5VRwQ3EIAwzpy7j2WCcdjazje9DEOWiq5pXsOw4McU23tQHL+sm6L2ndjc8VpJkST+CgVXbsI1iO51MskgyyQbgmreOpi4ONd6jhySHA5aUgoSV/E+AffKjICPvgmMFSTZJJ4BzC6DF4JlSpJE5pCk9/XCs9AXp0pyoA0mhyQAAAABJRU5ErkJggg==";
 
     private static readonly GUIContent kGithubMenuItemContent = new GUIContent("Github page");
     //private static readonly GUIContent kAssetStoreMenuItemContent = new GUIContent("AssetStore page");
     private static readonly GUIContent kResetButtonContent = new GUIContent(string.Empty, "Reset");
-    private static readonly GUIContent kPauseButtonContent = new GUIContent(string.Empty, "Pause");
-    private static readonly GUIContent kCanResetButtonContent = new GUIContent("R");
-    private static readonly GUIContent kCanSuppressButtonContent = new GUIContent("S");
     private static readonly GUIContent kPlayModeTooltipContent = new GUIContent();
     private static readonly GUIContent kEditModeTooltipContent = new GUIContent("", "Does not affect Time.timeScale while in EditorMode");
-    private static readonly GUIContent kCanResetToggleTooltipContent = new GUIContent("", "Auto-reset chronoScale to value set in EditorMode?");
-    private static readonly GUIContent kCanSuppressTimeScaleToggleTooltipContent = new GUIContent("", "Suppress Time.timeScale changes from without?");
+
+    private static readonly GUIContent kPauseButtonNormalContent = new GUIContent(string.Empty, "Pause");
+    private static readonly GUIContent kPauseButtonPressedContent = new GUIContent(string.Empty, "Pause");
+    private static readonly GUIContent kLockedButtonContent = new GUIContent(string.Empty, "Suppress Time.timeScale changes from without");
+    private static readonly GUIContent kUnlockedButtonContent = new GUIContent(string.Empty, "Allow Time.timeScale changes from without");
+    private static readonly GUIContent kAutoResetOnButtonContent = new GUIContent(string.Empty, "Auto-reset chronoScale to value set in EditorMode");
+    private static readonly GUIContent kAutoResetOffButtonContent = new GUIContent(string.Empty, "Don't auto-reset chronoScale to value set in EditorMode");
+
     private static readonly GUILayoutOption kControlButtonWidth = GUILayout.Width(38.0f);
     private static readonly GUILayoutOption kControlButtonHeight = GUILayout.Height(20.0f);
-    private static readonly GUILayoutOption kControlToggleWidth = GUILayout.Width(20.0f);
+    private static readonly GUILayoutOption kControlToggleWidth = GUILayout.Width(24.0f);
+    private static readonly GUILayoutOption kControlToggleHeight = GUILayout.Height(16.0f);
     private static readonly GUILayoutOption kControlToggleExpandWidth = GUILayout.ExpandWidth(false);
     //private static readonly GUILayoutOption kChronoSliderMinWidth = GUILayout.MinWidth(256.0f);
     private static readonly GUILayoutOption kChronoSliderMaxWidth = GUILayout.MaxWidth(8192.0f);
     private static readonly GUILayoutOption kChronoSliderExpandWidth = GUILayout.ExpandWidth(true);
     #endregion
 
-    private static Texture2D _resetIconTexture;
-    private static Texture2D _pauseIconTexture;
+    private static Texture2D _resetIconDarkTexture;
+    private static Texture2D _resetIconLightTexture;
+    private static Texture2D _pauseIconDarkTexture;
+    private static Texture2D _pauseIconLightTexture;
+    private static Texture2D _lockedIconLightTexture;
+    private static Texture2D _unlockedIconDarkTexture;
+    private static Texture2D _unlockedIconLightTexture;
+    private static Texture2D _autoResetOnIconLightTexture;
+    private static Texture2D _autoResetOffIconDarkTexture;
+    private static Texture2D _autoResetOffIconLightTexture;
 
     private bool _isPlayMode = false;
     private bool _canResetOnPlayEnd = true;
@@ -176,8 +217,7 @@ public class ChronoHelper : EditorWindow
     private void OnDisable()
     {
         EditorApplication.playmodeStateChanged -= OnApplicationStateChanged;
-        DestroyImmediate(_resetIconTexture);
-        DestroyImmediate(_pauseIconTexture);
+        ReleaseTextureContent();
     }
 
     private void OnDestroy()
@@ -202,22 +242,58 @@ public class ChronoHelper : EditorWindow
 
     private void InitTextureContent()
     {
-        _resetIconTexture = CreateTextureFromBase64(kResetIconBase64, "CH_Icon_Reset");
-        _pauseIconTexture = CreateTextureFromBase64(kPauseIconBase64, "CH_Icon_Pause");
-        kResetButtonContent.image = _resetIconTexture;
-        kPauseButtonContent.image = _pauseIconTexture;
+        _resetIconDarkTexture = CreateTextureFromBase64(kResetIcon_dark, "CH_Icon_Reset_Dark");
+        _pauseIconDarkTexture = CreateTextureFromBase64(kPauseIcon_dark, "CH_Icon_Pause_Dark");
+        _pauseIconLightTexture = CreateTextureFromBase64(kPauseIcon_light, "CH_Icon_Pause_Light");
+        _lockedIconLightTexture = CreateTextureFromBase64(kLockedIcon_light, "CH_Icon_Locked_Light");
+        _unlockedIconDarkTexture = CreateTextureFromBase64(kUnlockedIcon_dark, "CH_Icon_Unlocked_Dark");
+        _unlockedIconLightTexture = CreateTextureFromBase64(kUnlockedIcon_light, "CH_Icon_Unlocked_Light");
+        _autoResetOnIconLightTexture = CreateTextureFromBase64(kAutoResetOnIcon_light, "CH_Icon_AutoResetOn_Light");
+        _autoResetOffIconDarkTexture = CreateTextureFromBase64(kAutoResetOffIcon_dark, "CH_Icon_AutoResetOff_Dark");
+        _autoResetOffIconLightTexture = CreateTextureFromBase64(kAutoResetOffIcon_light, "CH_Icon_AutoResetOff_Light");
+
+        if (EditorGUIUtility.isProSkin)
+        {
+            kResetButtonContent.image = _resetIconDarkTexture;
+            kPauseButtonNormalContent.image = _pauseIconLightTexture;
+            kPauseButtonPressedContent.image = _pauseIconLightTexture;
+            kLockedButtonContent.image = _lockedIconLightTexture;
+            kUnlockedButtonContent.image = _unlockedIconLightTexture;
+            kAutoResetOnButtonContent.image = _autoResetOnIconLightTexture;
+            kAutoResetOffButtonContent.image = _autoResetOffIconLightTexture;
+        }
+        else
+        {
+            kResetButtonContent.image = _resetIconDarkTexture;
+            kPauseButtonNormalContent.image = _pauseIconDarkTexture;
+            kPauseButtonPressedContent.image = _pauseIconLightTexture;
+            kLockedButtonContent.image = _lockedIconLightTexture;
+            kUnlockedButtonContent.image = _unlockedIconDarkTexture;
+            kAutoResetOnButtonContent.image = _autoResetOnIconLightTexture;
+            kAutoResetOffButtonContent.image = _autoResetOffIconDarkTexture;
+        }
+    }
+
+    private void ReleaseTextureContent()
+    {
+        DestroyImmediate(_resetIconDarkTexture);
+        DestroyImmediate(_pauseIconDarkTexture);
+        DestroyImmediate(_lockedIconLightTexture);
+        DestroyImmediate(_unlockedIconDarkTexture);
+        DestroyImmediate(_autoResetOnIconLightTexture);
+        DestroyImmediate(_autoResetOffIconDarkTexture);
     }
 
     private void CreateChronoButtons()
     {
         _chronoButtons = new ChronoButton[]{
-            new ChronoButton(this, 0.000f, kPauseButtonContent),
-            new ChronoButton(this, 0.125f, kOneEighthButtonTitle),
-            new ChronoButton(this, 0.250f, kOneFourthButtonTitle),
-            new ChronoButton(this, 0.500f, kHalfButtonTitle),
-            new ChronoButton(this, 1.000f, kOneButtonTitle),
-            new ChronoButton(this, 1.500f, kOneAndHalfButtonTitle),
-            new ChronoButton(this, 2.000f, kTwiceButtonTitle)
+            new ChronoButton(this, 0.000f, kPauseButtonNormalContent, kPauseButtonPressedContent),
+            new ChronoButton(this, 0.125f, new GUIContent(kOneEighthButtonTitle)),
+            new ChronoButton(this, 0.250f, new GUIContent(kOneFourthButtonTitle)),
+            new ChronoButton(this, 0.500f, new GUIContent(kHalfButtonTitle)),
+            new ChronoButton(this, 1.000f, new GUIContent(kOneButtonTitle)),
+            new ChronoButton(this, 1.500f, new GUIContent(kOneAndHalfButtonTitle)),
+            new ChronoButton(this, 2.000f, new GUIContent(kTwiceButtonTitle))
         };
     }
 
@@ -289,6 +365,7 @@ public class ChronoHelper : EditorWindow
 
             for (int i = 0; i < _chronoButtons.Length; ++i)
             {
+                GUILayout.Space(-2.0f);
                 _chronoButtons[i].Draw();
             }
             UpdateChronoButtons();
@@ -301,10 +378,17 @@ public class ChronoHelper : EditorWindow
     {
         using (new EditorGUILayout.HorizontalScope())
         {
-            _canResetOnPlayEnd = GUILayout.Toggle(_canResetOnPlayEnd, kCanResetButtonContent, EditorStyles.miniButtonLeft, kControlToggleWidth, kControlToggleExpandWidth);
-            DrawTooltipOverLastRect(kCanResetToggleTooltipContent);
-            _canSuppressTimeScale = GUILayout.Toggle(_canSuppressTimeScale, kCanSuppressButtonContent, EditorStyles.miniButtonRight, kControlToggleWidth, kControlToggleExpandWidth);
-            DrawTooltipOverLastRect(kCanSuppressTimeScaleToggleTooltipContent);
+            var options = new GUILayoutOption[] { kControlToggleWidth, kControlToggleHeight, kControlToggleExpandWidth };
+
+            var autoResetButtonContent = _canResetOnPlayEnd
+                                       ? kAutoResetOnButtonContent
+                                       : kAutoResetOffButtonContent;
+            _canResetOnPlayEnd = GUILayout.Toggle(_canResetOnPlayEnd, autoResetButtonContent, EditorStyles.miniButtonLeft, options);
+
+            var suppressButtonContent = _canSuppressTimeScale
+                                      ? kLockedButtonContent
+                                      : kUnlockedButtonContent;
+            _canSuppressTimeScale = GUILayout.Toggle(_canSuppressTimeScale, suppressButtonContent, EditorStyles.miniButtonRight, options);
         }
     }
 
